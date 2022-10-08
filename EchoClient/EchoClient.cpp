c7b44d1edea40fdf025f08f619b236c0669d3cd7
Alexandr <kajlaleksandr1986@gmail.com>
2022-10-08 11:33:15 +0700

    commit

--- EchoClient/EchoClient.cpp
+++ EchoClient/EchoClient.cpp
@@ -4,180 +4,35 @@
 
 EchoClient::EchoClient(const uint16_t WRITE_PORT)
 {
-    //const char IP_SERV[] = "127.0.0.1";
-    //servinfo = get_addrinfo(get_nameinfo(IP_SERV));
-    servinfo = get_addrinfo("localhost");
-
-    for (auto const* s = servinfo; s != nullptr; s = s->ai_next)
-    {
-
-        if (AF_INET == s->ai_family)
-        {
-            char ip[INET_ADDRSTRLEN];
-
-            sockaddr_in* const sin = reinterpret_cast<sockaddr_in* const>(s->ai_addr);
-            in_addr addr;
-            addr.s_addr = *reinterpret_cast<const in_addr_t*>(&sin->sin_addr);
-
-            sin->sin_family = AF_INET;
-            sin->sin_port = htons(WRITE_PORT);
-
-            std::cout << "Trying IP Address: " << inet_ntop(AF_INET, &addr, ip, INET_ADDRSTRLEN) << std::endl;
-            socket_wrapper::Socket s{ AF_INET, SOCK_STREAM, IPPROTO_TCP };
-
-            if (try_to_connect(s, reinterpret_cast<const sockaddr*>(sin), sizeof(sockaddr_in)))
-            {
-                client_sock = new socket_wrapper::Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-                if (init_client(WRITE_PORT) == EXIT_FAILURE)
-                {
-                    std::cout << "Connection ipv4 failed\n";
-                    return;
-                }
-            }
-            
-            delete client_sock;
-
-        }
-        else if (AF_INET6 == s->ai_family)
-        {
-            char ip6[INET6_ADDRSTRLEN];
-
-            sockaddr_in6* const sin = reinterpret_cast<sockaddr_in6* const>(s->ai_addr);
-
-            sin->sin6_family = AF_INET6;
-            sin->sin6_port = htons(WRITE_PORT);
-
-            std::cout << "Trying IPv6 Address: " << inet_ntop(AF_INET6, &(sin->sin6_addr), ip6, INET6_ADDRSTRLEN) << std::endl;
-            socket_wrapper::Socket s{ AF_INET6, SOCK_STREAM, IPPROTO_TCP };
-
-            if (try_to_connect(s, reinterpret_cast<const sockaddr*>(sin), sizeof(sockaddr_in6)))
-            {
-                client_sock = new socket_wrapper::Socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
-                if (init_client6(WRITE_PORT) == EXIT_FAILURE)
-                {
-                    std::cout << "Connection ipv6 failed\n";
-                    return;
-                }
-            }
-
-            delete client_sock;
-        }
-    }
-
-    //if (servinfo->ai_family == AF_INET)
-    //{
-    //    if (init_client(WRITE_PORT) == EXIT_FAILURE)
-    //    {
-    //        std::cout << "Connection ipv4 failed\n";
-    //        return;
-    //    }
-    //}
-    //else if (servinfo->ai_family == AF_INET6)
-    //{
-    //    if (init_client6(WRITE_PORT) == EXIT_FAILURE)
-    //    {
-    //        std::cout << "Connection ipv6 failed\n";
-    //        return;
-    //    }
-    //}
-
-    write();
+    write(WRITE_PORT);
 }
 
 EchoClient::~EchoClient()
 {
-    freeaddrinfo(servinfo);
-    //delete client_sock;
 }
 
-bool EchoClient::try_to_connect(socket_wrapper::Socket& s, const sockaddr* sa, size_t sa_size)
+int EchoClient::write(const uint16_t WRITE_PORT)
 {
-    if (connect(s, sa, sa_size) == -1)
-    {
-        std::cerr << "Connection failed: " << sock_wrap.get_last_error_string() << std::endl;
-        return false;
-    }
-
-    s.close();
-    return true;
-}
-
-//Метод для трансяции имени хоста в адресс
-//Возвращает указатель на связанный список структур addrinfo содержащую информацию о хосте
-addrinfo* EchoClient::get_addrinfo(const std::string& host_name)
-{
-    // Need for Windows initialization.
-    //socket_wrapper::SocketWrapper sock_wrap;
-
-    addrinfo* servinfo = nullptr;
-
-    addrinfo hints =
-    {
-        .ai_flags = AI_CANONNAME,
-        // Неважно, IPv4 или IPv6.
-        .ai_family = AF_UNSPEC,
-        // TCP stream-sockets.
-        .ai_socktype = SOCK_STREAM,
-        // Any protocol.
-        .ai_protocol = 0
-    };
-
-    int status{};
-
-    if ((status = getaddrinfo(host_name.c_str(), nullptr, &hints, &servinfo)) != 0)
-    {
-        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
-        return nullptr;
-    }
-
-    return servinfo;
-}
-
-//Метод для получения от сервера имени хоста по ip адресу
-//Возвращает строку с именем хоста
-char* EchoClient::get_nameinfo(const std::string& ip_addr)
-{
-    // Need for Windows initialization.
-    //socket_wrapper::SocketWrapper sock_wrap;
-    sockaddr_in pSockaddr;
-
-    pSockaddr.sin_family = AF_INET;
-    pSockaddr.sin_addr.s_addr = inet_addr(ip_addr.c_str());
-
-    char host_name[NI_MAXHOST]{};
-
-    //std::string hostName;
-    int status{};
-    if (status = getnameinfo((sockaddr*)&pSockaddr, sizeof(sockaddr), host_name, NI_MAXHOST, NULL, NULL, NULL) != 0)
-    {
-        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
-        return nullptr;
-    }
-
-    return host_name;
-}
-
-int EchoClient::init_client(const uint16_t WRITE_PORT)
-{   
     std::cout << "Starting echo client ...\n";
 
-    //client_sock = new socket_wrapper::Socket( AF_INET, SOCK_STREAM, NULL);
+    socket_wrapper::SocketWrapper sock_wrap;
+    socket_wrapper::Socket client_sock = { AF_INET, SOCK_STREAM, NULL };
+    const char IP_SERV[] = "127.0.0.1";
 
-    if (!*client_sock)
+    if (!client_sock)
     {
         std::cerr << sock_wrap.get_last_error_string() << std::endl;
         return EXIT_FAILURE;
     }
 
-    sockaddr_in* const sin = reinterpret_cast<sockaddr_in* const>(servinfo->ai_addr);
-    in_addr addr;
-    addr.s_addr = *reinterpret_cast<const in_addr_t*>(&sin->sin_addr);
-    sin->sin_family = AF_INET;
-    sin->sin_port = htons(WRITE_PORT);
+    sockaddr_in serv_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(WRITE_PORT)
+    };
 
+    serv_addr.sin_addr.s_addr = inet_addr(IP_SERV);
 
-    if (connect(*client_sock, (SOCKADDR*)sin, sizeof(sockaddr_in)))
+    if (connect(client_sock, (SOCKADDR*)&serv_addr, sizeof(serv_addr)))
     {
         std::cout << "Connection to Server is FAILED. Error #";
         std::cerr << sock_wrap.get_last_error_string() << std::endl;
@@ -187,40 +42,11 @@ int EchoClient::init_client(const uint16_t WRITE_PORT)
     //
     std::cout << "Connected! ...\n";
 
-    return EXIT_SUCCESS;
-}
+    //std::string buffer;
+    //char recv_buffer[256]{};
+    //int recv_len{};
+    //socklen_t client_addrlen = sizeof(sockaddr_in);
 
-int EchoClient::init_client6(const uint16_t WRITE_PORT)
-{
-    std::cout << "Starting echo client ...\n";
-
-    client_sock = new socket_wrapper::Socket(AF_INET6, SOCK_STREAM, NULL);
-
-    if (!*client_sock)
-    {
-        std::cerr << sock_wrap.get_last_error_string() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    sockaddr_in6* const sin = reinterpret_cast<sockaddr_in6* const>(servinfo->ai_addr);
-
-    sin->sin6_family = AF_INET6;
-    sin->sin6_port = htons(WRITE_PORT);
-
-    if (connect(*client_sock, (SOCKADDR*)sin, sizeof(sockaddr_in6)))
-    {
-        std::cout << "Connection to Server is FAILED. Error #";
-        std::cerr << sock_wrap.get_last_error_string() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::cout << "Connected! ...\n";
-
-    return EXIT_SUCCESS;
-}
-
-int EchoClient::write()
-{
     std::vector<char>buff(BUFF_SIZE);
     std::string send_mess;
     std::string send_buff;
@@ -230,11 +56,20 @@ int EchoClient::write()
     while (true)
     {
         std::cout << "Your (Client) message to Server: ";
+        //std::cin >> send_mess;
         fgets(buff.data(), buff.size(), stdin);
+
+        //send_buff.reserve(send_mess.size() + 2);
+        //uint8_t sz_l = send_mess.size();
+        //uint8_t sz_h = (send_mess.size() & 0xFFFF) >> 8;
+
+        //send_buff.at(0) = sz_l;
+        //send_buff.at(1) = sz_h;
+        //send_buff.insert(2, send_mess);
 
         send_mess = buff.data();
 
-        packet_size = send(*client_sock, send_mess.c_str(), send_mess.size() - 1, 0);
+        packet_size = send(client_sock, send_mess.c_str(), send_mess.size() - 1, 0);
 
         if (packet_size == SOCKET_ERROR)
         {
@@ -243,15 +78,15 @@ int EchoClient::write()
             return EXIT_FAILURE;
         }
 
-        if (str_tolower(send_mess) == CMD_EXT)
+        if (send_mess == "exit\n")
         {
-            shutdown(*client_sock, SD_BOTH);
+            shutdown(client_sock, SD_BOTH);
             std::cout << "Stopping echo server ...\n";
             std::cout << "Disconected\n";
-            return EXIT_SUCCESS;
+            return 0;
         }
 
-        packet_size = recv(*client_sock, buff.data(), buff.size(), 0);
+        packet_size = recv(client_sock, buff.data(), buff.size(), 0);
 
         if (packet_size == SOCKET_ERROR)
         {
@@ -261,17 +96,34 @@ int EchoClient::write()
         }
         else
             std::cout << "Server message: " << buff.data() << std::endl;
+
+        //std::cin >> buffer;
+
+        //sendto(client_sock, buffer.c_str(), buffer.size(), 0, reinterpret_cast<const sockaddr*>(&serv_addr),
+        //    sizeof(serv_addr));
+
+        //str_tolower(buffer);
+
+        //if (buffer == CMD_EXT)
+        //{
+        //    std::cout << "Stoped echo clients ...\n";
+        //    break;
+        //}
+
+        //recv_len = recv(client_sock, recv_buffer, sizeof(recv_buffer) - 1, 0);
+
+        //std::cout << recv_buffer << std::endl;
+
+
     }
 
     return EXIT_SUCCESS;
 }
 
-std::string EchoClient::str_tolower(const std::string& str)
+void EchoClient::str_tolower(std::string& str)
 {
-    std::string str_lower;
-    for (int i{}; i < str.size() - 1; ++i)
-        str_lower += tolower(str[i]);
+    for (int i{}; i < str.size(); ++i)
+        str[i] = tolower(str[i]);
 
-    return str_lower;
 }
 
